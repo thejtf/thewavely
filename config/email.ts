f4c009bdd75160f69b94a7ba672f0c89d0ffb550
Jopus <chizongjtf@gmail.com>
2023-07-18 13:19:22 +0800

    release

--- config/email.ts
+++ config/email.ts
@@ -1,7 +1,7 @@
 export const emailConfig = {
-  from: 'hi@cali.so',
+  from: 'chizong@gmail.com',
   baseUrl:
     process.env.VERCEL_ENV === 'production'
-      ? `https://cali.so`
+      ? `https://thewavely.com`
       : 'http://localhost:3000',
 }
