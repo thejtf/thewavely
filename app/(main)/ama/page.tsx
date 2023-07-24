c55b9c4a0f1739e2011a15f815fe2a9619b1a4e0
Jopus <chizongjtf@gmail.com>
2023-07-24 20:32:43 +0800

    release

--- app/(main)/ama/page.tsx
+++ app/(main)/ama/page.tsx
@@ -31,7 +31,7 @@ export default function AskMeAnythingPage() {
     <Container className="mt-16 sm:mt-24">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Reading Club / 外刊精读营
+          Reading Club / 精读营
         </h1>
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
