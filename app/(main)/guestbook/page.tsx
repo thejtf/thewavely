dc89d2cbed542920ac83fb255834af1ca44be481
Jopus <chizongjtf@gmail.com>
2023-07-30 22:00:59 +0800

    release

--- app/(main)/guestbook/page.tsx
+++ app/(main)/guestbook/page.tsx
@@ -30,7 +30,7 @@ export default async function GuestBookPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          欢迎来到我的打卡墙
+          欢迎来到打卡墙
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
