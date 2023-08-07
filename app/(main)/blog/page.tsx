9d57d8ff962dd7990785b7db8f888954f52221e2
Jopus <chizongjtf@gmail.com>
2023-08-07 22:00:02 +0800

    release

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -6,7 +6,7 @@ import { Container } from '~/components/ui/Container'
 import { BlogPosts } from './BlogPosts'
 
 const description =
-  '阅读外刊是提高英语水平，获取丰富多样性信息的重要途径。外刊文章语言地道流畅，不仅可以增强英语阅读和写作能力，还可以提高我们的英语思维和应考能力。浪潮外刊收录涵盖全球政治、经济、文化等多个领域权威杂志期刊，下面是近期我们的收录更新。'
+  '阅读外刊是提高英语水平，获取丰富多样性信息的重要途径。外刊文章语言地道流畅，不仅可以增强英语阅读和写作能力，还可以提高我们的英语思维和应考能力。浪潮外刊收录涵盖全球经济、社会、文化等多个领域权威杂志期刊，下面是近期的收录更新。'
 export const metadata = {
   title: '最新外刊',
   description,
