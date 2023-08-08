2a4997addeb0b3db9c1038fba21d857519f96c59
Jopus <chizongjtf@gmail.com>
2023-08-09 00:29:46 +0800

    release

--- app/(main)/ama/page.tsx
+++ app/(main)/ama/page.tsx
@@ -87,7 +87,7 @@ export default function AskMeAnythingPage() {
         <p>
           添加申请时请注明来意为{' '}
           <RichLink
-            href="https://wj.qq.com/s2/12892874/3909/"
+            href="https://survey.xcardapp.com/s/w8iTY1"
             target="_blank"
           >
             加入精读营
