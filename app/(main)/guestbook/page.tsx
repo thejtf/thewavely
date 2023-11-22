745ac7bd43de6bd102f4e0846661454d29b6f183
Jopus <chizongjtf@gmail.com>
2023-11-22 23:32:08 +0800

    release

--- app/(main)/guestbook/page.tsx
+++ app/(main)/guestbook/page.tsx
@@ -6,9 +6,9 @@ import { fetchGuestbookMessages } from '~/db/queries/guestbook'
 
 import { Guestbook } from './Guestbook'
 
-const title = '打卡墙'
+const title = '留言墙'
 const description =
-  '在这里，你可以留下你想说的话，每日学习打卡、分享交流均可。让我们自己认识到语言工具的重要性，我们才能忍受那漫长的、难熬的、延迟满足的学习过程。希望我们大家的彼此交流打气激励着我们共同无限进步。'
+  '在这里，你可以留下你想说的话，学习分享交流也可。我们会认真听取大家给我们的真诚的建议，让我们共同无限进步。'
 export const metadata = {
   title,
   description,
@@ -30,7 +30,7 @@ export default async function GuestBookPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          欢迎来到打卡墙
+          欢迎来到留言墙
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
