3216be44d3bcb27b0f2d2b3f950729b513bd4746
Jopus <chizongjtf@gmail.com>
2023-11-22 23:19:48 +0800

    release

--- config/nav.ts
+++ config/nav.ts
@@ -2,7 +2,7 @@ export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '公告' },
   { href: '/projects', text: '收录' },
-  { href: '/guestbook', text: '打卡墙' },
+  { href: '/guestbook', text: '留言墙' },
   { href: '/ama', text: '精读营' },
   // { href: '/about', text: '关于' },
 ]
